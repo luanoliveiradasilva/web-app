7a8c59f25d5f64926a2fa4a6ba07d90346306927
Luan Nascimento <luanoliveira1107@gmail.com>
2022-03-01 19:00:37 -0300

    [https://github.com/luanoliveiradasilva/web-app/projects/1#card-78445150] - criar de use cases

--- src/main/java/com/example/webapp/usecases/Pessoas/PessoaUseCases.java
+++ src/main/java/com/example/webapp/usecases/Pessoas/PessoaUseCases.java
@@ -1,21 +1,28 @@
-package com.example.webapp.usecases;
+package com.example.webapp.usecases.Pessoas;
 
 import com.example.webapp.model.Pessoa;
 import com.example.webapp.repository.PessoaRepo;
+import com.example.webapp.usecases.PessoaUseCasesImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 
 import java.util.List;
 
 @Service
-public class PessoaUseCases {
+public class PessoaUseCases implements PessoaUseCasesImpl {
 
     @Autowired
     private PessoaRepo pessoaRepo;
 
-    public List<Pessoa> pegarTodasAsPessoas(){
+    @Override
+    public List<Pessoa> findAll() {
         return pessoaRepo.findAll();
     }
+
+    @Override
+    public Pessoa postAll(Pessoa pessoas) {
+        return pessoaRepo.save(pessoas);
+    }
+
 
 }
