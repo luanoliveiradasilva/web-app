b77457f63e753b83c40ec6e1012a1ac2f4e137cc
Luan Nascimento <luanoliveira1107@gmail.com>
2022-03-06 21:12:07 -0300

    criar de use cases

--- src/main/java/com/example/webapp/controller/PessoasController.java
+++ src/main/java/com/example/webapp/controller/PessoasController.java
@@ -4,6 +4,10 @@ package com.example.webapp.controller;
 import com.example.webapp.model.Pessoa;
 import com.example.webapp.usecases.Pessoas.PessoaUseCases;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -11,20 +15,20 @@ import java.util.List;
 @RestController
 @RequestMapping("/cadastro/v1")
 public class PessoasController {
-
     @Autowired
     PessoaUseCases pessoasUseCases;
 
     @ResponseBody
-    @GetMapping("/formulario")
+    @GetMapping(value = "/formulario")
     public List<Pessoa> getPessoas() {
         return pessoasUseCases.findAll();
     }
 
     @ResponseBody
-    @PostMapping("/cadastro")
-    public Pessoa postPessoas(@RequestBody Pessoa pessoa) {
-        return pessoasUseCases.postAll(pessoa);
+    @PostMapping(value = "/cadastros")
+    public ResponseEntity<Pessoa> postPessoas(
+            @RequestBody Pessoa pessoa) {
+        var pessoaUse = pessoasUseCases.postAll(pessoa);
+        return new ResponseEntity(HttpStatus.OK);
     }
-
 }
